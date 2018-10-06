afc8b09f7365c66b32287a5c637859d60e98e5a4
nosyarg <york17a@ncssm.edu>
2018-10-05 22:33:03 -0700

    pushin

--- displaynotes.py
+++ displaynotes.py
@@ -1,11 +1,20 @@
 from abjad import *
-score = abjad.Score()
-staff = abjad.StaffGroup([], lilypond_type = 'PianoStaff')
+def rightnote():
+        print("WAS THE NOTE RIGHT?")
+        return int(input())
 line = abjad.Staff()
-staff.append(line)
-score.append(staff)
 for i in range(2):
         line.append(abjad.Measure((4,4),[]))
-line[0].extend("e'4 d'4 c'4 d'4")
-line[1].extend("e'4 e'4 e'2")
-show(score)
+line[0].extend("e''4 d''4 c''4 d''4")
+line[1].extend("e''4 e''4 e''2")
+allnotes = []
+for measure in line:
+        for note in measure:
+                allnotes.append(note)
+for i in range(len(allnotes)):
+        while(not rightnote()):
+                tweak(allnotes[i].note_head).color = 'red'
+                show(line)
+        tweak(allnotes[i].note_head).color = 'green'
+        show(line)
+        tweak(allnotes[i].note_head).color = 'black'
