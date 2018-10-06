28edfc944b50cdf003af4473fc10eeacb48a3d73
nosyarg <york17a@ncssm.edu>
2018-10-06 10:50:02 -0700

    pushin

---
+++ analyze.py
@@ -0,0 +1,109 @@
+import argparse
+import tempfile
+from queue import Queue
+import sys
+
+import os
+import pickle
+import time
+
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
+
+def device_list():
+    return sd.query_devices()
+
+class Recorder():
+    def __init__(self, out_fname, durations, sample_rate=44100, device=None, channels=1, debug=False):
+        self.out_fname = out_fname
+        self.durations = durations
+        self.sample_rate = sample_rate
+        self.device = device
+        self.channels = channels
+        self.debug = debug
+
+        self.data_queue = Queue()
+
+    def get_queue(self):
+        return self.data_queue
+
+    def fft_analyze(self, data, rate):
+        spectre = np.fft.fft(data)
+        freq = np.fft.fftfreq(data.size, 1/rate)
+        mask = freq > 0
+        return freq[np.argmax(np.abs(spectre[mask]))]
+
+    def record(self):
+        raw_queue = Queue()
+        full_arr = np.array([])
+        ret_arr = []
+
+        def callback(indata, frames, time, status):
+            """Audio callback"""
+            if status:
+                print(status, file=sys.stderr)
+            raw_queue.put((indata.copy(), time.inputBufferAdcTime))
+
+        with sf.SoundFile(self.out_fname, mode='x', samplerate=self.sample_rate,
+                          channels=self.channels, subtype=None) as file:
+            with sd.InputStream(samplerate=self.sample_rate, device=self.device,
+                                channels=self.channels, callback=callback) as ss:
+                start_time = ss.time
+                next_slice_start = None
+                dur_count = 0
+                while True:
+                    np_arr, buff_time = raw_queue.get()
+                    if next_slice_start is None:
+                        next_slice_start = int((buff_time-start_time)*self.sample_rate)
+                    file.write(np_arr)
+                    full_arr = np.append(full_arr, np_arr)
+                    if full_arr.size >= int(next_slice_start+self.durations[dur_count]*self.sample_rate+1):
+                        if self.debug:
+                            print('Note Info: ({} num) ({} dur)'.format(dur_count, 
+                                self.durations[dur_count]))
+                        slice_end = int(next_slice_start+self.durations[dur_count]*self.sample_rate)
+                        raw_slice = full_arr[next_slice_start:slice_end]
+                        self.data_queue.put(self.fft_analyze(raw_slice, self.sample_rate))
+                        if self.debug:
+                            ret_arr.append(raw_slice)
+                        next_slice_start = slice_end+1
+                        dur_count+=1
+                        if dur_count == len(self.durations):
+                            # if self.debug:
+                            #     now = time.time()
+                            #     debug_file = tempfile.mktemp(prefix='debug_slices_{}_'.format(now), suffix='.pkl', dir='')
+                            #     with open(debug_file, 'wb') as fopen:
+                            #         pickle.dump(debug_file, fopen)
+                            self.data_queue.put(-1)
+                            return
+
+if __name__ == '__main__':
+    import threading
+    now = time.time()
+    durations = [0.5, 0.5, 0.5, 0.5, 
+                0.5, 0.5, 1, 
+                0.5, 0.5, 1, 
+                0.5, 0.5, 1, 
+                0.5, 0.5, 0.5, 0.5, 
+                0.5, 0.5, 0.5, 0.5,
+                0.5, 0.5, 0.5, 0.5,
+                2]
+    fname = 'nolanmary.wav'
+    try:
+        os.remove(fname)
+    except OSError:
+        pass
+    rec = Recorder(out_fname=fname, durations=durations, debug=True)
+
+    data_queue = rec.get_queue()
+
+    threading.Thread(target=rec.record).start()
+
+    while True:
+        data = data_queue.get()
+        print(data)
+        if data == -1:
+            break
+
+    
--- displaynotes.py
+++ displaynotes.py
@@ -1,11 +1,13 @@
 from abjad import *
+from math import *
 import time
 from analyze import *
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import sys
+import threading
+"""
 def getfreq(lens):
-    import threading
     now = time.time()
     rec = Recorder(out_fname='test_{}.wav'.format(now), durations=lens, debug=True)
 
@@ -17,6 +19,7 @@ def getfreq(lens):
         print('Enter')
         data = data_queue.get()
         return data
+"""
 numnotes = int(sys.argv[2])
 plt.rcParams["figure.figsize"] = [20.0,9.0]
 datafile = open(sys.argv[1] + '/data.csv','r')
@@ -25,27 +28,37 @@ freqs = []
 for line in datafile:
         lens.append(float(line.split(',')[0]))
         freqs.append(float(line.split(',')[1]))
-def rightnote():
-        print(getfreq(lens))
-        print("WAS THE NOTE RIGHT?")
-        return int(input())
+def rightnote(truth):
+        return abs(data_queue.get() - truth) < 20
 image = mpimg.imread(sys.argv[1] + '/ground.png')
 image = image[30:120,:,:]
 plt.imshow(image)
 plt.ion()
 plt.show()
+fname = 'nolanmary.wav'
+try:
+        os.remove(fname)
+except OSError:
+        pass
+rec = Recorder(out_fname=fname,durations=lens, debug=True)
+data_queue = rec.get_queue()
+for i in range(4):
+        print("\7")
+        time.sleep(.5)
+threading.Thread(target=rec.record).start()
 for i in range(numnotes):
-        while(not rightnote()):
+        if(not rightnote(freqs[i])):
                 image = mpimg.imread(sys.argv[1] + '/' + str(i) + 'wrong.png')
                 image = image[30:120,:,:]
-                #plt.imshow(image,aspect = 'auto')
                 plt.imshow(image)
+                plt.pause(.001)
                 plt.ion()
                 plt.show()
-        image = mpimg.imread(sys.argv[1] + '/' + str(i) + 'right.png')
-        image = image[30:120,:,:]
-        plt.imshow(image)
-        plt.pause(.001)
-        plt.ion()
-        plt.show()
+        else:
+                image = mpimg.imread(sys.argv[1] + '/' + str(i) + 'right.png')
+                image = image[30:120,:,:]
+                plt.imshow(image)
+                plt.pause(.001)
+                plt.ion()
+                plt.show()
  
